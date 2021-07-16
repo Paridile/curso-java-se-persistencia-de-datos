b4c842c82990a52cde81e14cc40525e0dca9f6af
Pablo Diaz de Leon <pablorios1603@gmail.com>
2021-07-16 11:47:03 -0500

    Clase 11 -> insercion de datos

--- mensajes_app/src/main/java/com/paridile/mensajes_app/MensajesDAO.java
+++ mensajes_app/src/main/java/com/paridile/mensajes_app/MensajesDAO.java
@@ -5,17 +5,37 @@
  */
 package com.paridile.mensajes_app;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 /**
  *
  * @author j.rios.diaz.de.leon
  */
 public class MensajesDAO {
     public static void crearMensajeDB(Mensajes mensaje) {
-    
+        Conexion db_connect = new Conexion();
+        try(Connection conexion = db_connect.getConnection()) {
+            PreparedStatement ps = null;
+            try {
+               String query = "INSERT INTO `mensajes` (`mensaje`, `autor`, `fecha_mensaje`) VALUES (?,?, current_timestamp())";
+               ps= conexion.prepareStatement(query);
+               ps.setString(1, mensaje.getMensaje());
+               ps.setString(2, mensaje.getAutor_mensaje());
+               ps.executeUpdate();
+                System.out.println("Mensaje creado");
+            }catch(SQLException e) {                
+                System.out.println("Ha ocurrido un error al insetar su mensaje");
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
     }
     
     public static void leerMensajesDB() {
-    
+  
     }
     
     public static void borrarMensajeDB(int id_mensaje) {
--- mensajes_app/src/main/java/com/paridile/mensajes_app/MensajesService.java
+++ mensajes_app/src/main/java/com/paridile/mensajes_app/MensajesService.java
@@ -5,13 +5,26 @@
  */
 package com.paridile.mensajes_app;
 
+import java.util.Scanner;
+
 /**
  *
  * @author j.rios.diaz.de.leon
  */
 public class MensajesService {
     public static void crearMensaje() {
-    
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu mensaje");
+        String mensaje = sc.nextLine();
+        
+        System.out.println("Escribe tu nombre");
+        String nombre = sc.nextLine();
+        
+        Mensajes registro = new Mensajes();
+        registro.setMensaje(mensaje);
+        registro.setAutor_mensaje(nombre);
+        
+        MensajesDAO.crearMensajeDB(registro); 
     }
     
     public static void listarMensajes() {
