0c42dcdda07b49540e4b7f5a3e0318a7e9f7d6c7
Pablo Diaz de Leon <pablorios1603@gmail.com>
2021-07-16 10:14:24 -0500

    Clase 7 -> Creación del proyecto

---
+++ mensajes_app/src/main/java/com/paridile/mensajes_app/Inicio.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.paridile.mensajes_app;
+
+/**
+ *
+ * @author j.rios.diaz.de.leon
+ */
+public class Inicio {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
